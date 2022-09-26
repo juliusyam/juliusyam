74692a39bd69487be7692a9cf2719cb5775623bb
juliusyam <juliusworldwide@gmail.com>
2022-09-26 19:29:24 +0100

    updated implementation of home page landing

--- components/Landing/index.tsx
+++ components/Landing/index.tsx
@@ -5,33 +5,22 @@ export function LandingInitialSlide() {
 
   return (
     <div className="section relative h-screen">
-      <div className="grid grid-cols-2 h-screen">
-        <LandingImageGrid/>
-        <LandingText/>
+      <div className="grid place-items-center px-5 md:px-20 w-full h-screen">
+        <section className="relative w-full max-w-9xl h-full">
+          <div className="absolute bottom-0 left-0 mt-96">
+            <Image src="/img/profile.png" layout="intrinsic" objectFit="cover" width="800" height="700" />
+          </div>
+
+          <div className="absolute right-0 bottom-1/2 translate-y-1/2">
+            <JuliusYam textAlign="text-right" fontSize="text-3xl md:text-5xl lg:text-8xl text-right" />
+            <h2 className="font-ocr text-jy-cyan text-2xl text-right">Web, Mobile, UX, Graphic Design</h2>
+          </div>
+
+          <h5 className="font-ocr text-gray-400 text-1xl text-right bottom-20 right-0 absolute w-80">
+            Copyright © 2022 by Julius Yam. All Rights Reserved
+          </h5>
+        </section>
       </div>
     </div>
-  )
-}
-
-function LandingImageGrid() {
-  return (
-    <section className="flex justify-end items-end">
-      <Image src="/img/profile.png" layout="fixed" width="800" height="710" objectFit="cover" />
-    </section>
-  )
-}
-
-function LandingText() {
-  return (
-    <section className="p-20 grid content-center align-center relative">
-      <div>
-        <JuliusYam textAlign="text-right" fontSize="text-3xl md:text-5xl lg:text-8xl text-right" />
-        <h2 className="font-ocr text-jy-cyan text-2xl text-right">Web, Mobile, UX, Graphic Design</h2>
-      </div>
-
-      <h5 className="font-ocr text-gray-400 text-1xl text-right bottom-20 right-20 absolute w-80">
-        Copyright © 2022 by Julius Yam. All Rights Reserved
-      </h5>
-    </section>
   )
 }
