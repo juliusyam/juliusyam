f207b7070a596cd6a9602883695ab92c2d48b07a
juliusyam <jyyam1999@gmail.com>
2022-10-09 20:05:28 +0100

    updates on global Head information

--- components/AppWrapper.tsx
+++ components/AppWrapper.tsx
@@ -2,28 +2,37 @@ import { Navigation } from './Navigation';
 import { useRouter } from 'next/router';
 import { AnimatePresence } from 'framer-motion';
 import { ChildrenProps } from '../models';
+import Head from "next/head";
 
 export function AppWrapper({ children }: ChildrenProps) {
 
   const { pathname } = useRouter();
 
-  if (pathname === '/') return <AnimationWrapper>{ children }</AnimationWrapper>;
+  if (pathname === '/') return <Wrapper>{ children }</Wrapper>;
 
   return (
     <>
       <div className="fixed w-full z-50"><Navigation /></div>
 
       <div>
-        <AnimationWrapper>{ children }</AnimationWrapper>
+        <Wrapper>{ children }</Wrapper>
       </div>
     </>
   )
 }
 
-function AnimationWrapper({ children }: ChildrenProps) {
+function Wrapper({ children }: ChildrenProps) {
   return (
-    <AnimatePresence exitBeforeEnter initial={ false }>
-      { children }
-    </AnimatePresence>
+    <>
+      <Head>
+        <title>Julius Yam | Web, Mobile, UX, Graphic Design</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
+        <meta name="description" content="Design is a function that transcends practicality into the world of beauty. The idea of a design focused software developer is to create bespoke software products that converts functionality into aesthetics and usability, whilst devising a design language uniquely tailored to each client, and ultimately transform unimaginable ideas to life."/>
+        <link rel="shortcut icon" href="/img/favicon.ico" />
+      </Head>
+      <AnimatePresence exitBeforeEnter initial={ false }>
+        { children }
+      </AnimatePresence>
+    </>
   )
 }
