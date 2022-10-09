331db56dff99d786d89ebd7c8597d99b479d1f44
juliusyam <jyyam1999@gmail.com>
2022-10-09 12:19:41 +0100

    useScroll initial implementation

--- components/Portfolio/PortfolioPageContainer.tsx
+++ components/Portfolio/PortfolioPageContainer.tsx
@@ -1,5 +1,7 @@
 import { ChildrenProps } from '../../models';
 import Image from "next/image";
+import {useScroll} from "../../hooks/useScroll";
+import {scale} from "style-value-types";
 
 interface PortfolioPageContainerProps extends ChildrenProps {
   title: string,
@@ -9,6 +11,9 @@ interface PortfolioPageContainerProps extends ChildrenProps {
 }
 
 export function PortfolioPageContainer({ title, backgroundColor, backgroundSrc, children, fontSize }: PortfolioPageContainerProps) {
+
+ const { ref, scrollProps: { componentHeight, scrollPosition } } = useScroll();
+
   return (
     <div className="grid grid-cols-1
     md:grid-cols-portfolio-display-md
@@ -16,19 +21,21 @@ export function PortfolioPageContainer({ title, backgroundColor, backgroundSrc, 
     xl:grid-cols-portfolio-display-xl
     w-full h-screen">
 
-      <div className="grid w-full h-48 md:h-full relative">
+      <div className="grid w-full h-48 md:h-full relative overflow-hidden">
         <Image src={ backgroundSrc }
                layout="responsive"
                objectFit="cover"
                width="100%"
-               height="100%" />
+               height="100%"
+               style={ { transform: `scale(${ componentHeight / (scrollPosition + 1) })` } }
+        />
 
         <div className={`absolute top-0 left-0 w-full h-full ${ backgroundColor } bg-opacity-80`} />
 
         <h1 className={`font-ocr drop-shadow-text absolute left-0 md:left-auto right-auto md:right-0 top-20 md:top-48 ${ fontSize || 'text-3xl xl:text-9xl' } w-full break-all text-left md:text-right p-4`}>{ title }</h1>
       </div>
 
-      <div className="grid grid-cols-1 gap-64 px-5 md:px-16 py-20 md:py-64 h-full overflow-y-scroll">
+      <div className="grid grid-cols-1 gap-64 px-5 md:px-16 py-20 md:py-64 h-full overflow-y-scroll" ref={ ref }>
         { children }
       </div>
     </div>
---
+++ hooks/useScroll.ts
@@ -0,0 +1,61 @@
+import { useEffect, useRef, useState } from 'react';
+
+enum ScrollDirection {
+  up = 'up',
+  down = 'down',
+}
+
+interface ScrollProps {
+  scrollDirection: ScrollDirection,
+  componentHeight: number,
+  scrollPosition: number,
+}
+
+const THRESHOLD = 0;
+
+export function useScroll() {
+
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [scrollProps, setScrollProps] = useState<ScrollProps>({
+    scrollDirection: ScrollDirection.down,
+    scrollHeight: 0,
+    componentHeight: 0,
+    scrollPosition: 0,
+  });
+
+  const prevScrollY = useRef(0);
+
+  const updateScroll = () => {
+    const current = ref?.current;
+    const scrollY = current?.scrollTop || 0;
+
+    if (Math.abs(scrollY - prevScrollY.current) >= THRESHOLD) {
+      const newScrollDirection = scrollY > prevScrollY.current ? ScrollDirection.down : ScrollDirection.up;
+
+      console.log("scrolling!!", newScrollDirection, scrollY, ref?.current?.scrollHeight - ref?.current?.clientHeight);
+
+      const scrollHeight = current?.scrollHeight || 0;
+      const clientHeight = current?.clientHeight || 0;
+
+      setScrollProps({
+        scrollDirection: newScrollDirection,
+        componentHeight: scrollHeight - clientHeight,
+        scrollPosition: scrollY,
+      });
+
+      prevScrollY.current = scrollY > 0 ? scrollY : 0;
+    }
+  }
+
+  useEffect(() => {
+    console.log('running');
+
+    const handleScroll = () => window?.requestAnimationFrame(updateScroll);
+
+    ref?.current?.addEventListener('scroll', handleScroll);
+
+    return () => ref?.current?.removeEventListener('scroll', handleScroll);
+  }, [scrollProps]);
+
+  return { ref, scrollProps };
+}
