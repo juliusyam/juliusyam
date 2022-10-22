367b3ad54a62acd4dac06fd0f89f1a7abf1b1815
juliusyam <juliusworldwide@gmail.com>
2022-10-22 15:36:51 +0100

    updated carousel controls

--- components/Carousel/BottomControls.tsx
+++ components/Carousel/BottomControls.tsx
@@ -11,9 +11,9 @@ export function CarouselBottomControls({ title, description, onClickLeft, onClic
 
   return (
     <div className="absolute left-0 bottom-0 grid grid-cols-1 lg:grid-cols-2 bg-jy-background w-full h-64 lg:h-36">
-      <section className="p-5 grid items-center">
-        <h1 className="font-ocr text-3xl pb-5 truncate">{ title }</h1>
-        <h2 className="font-ocr text-base text-gray-500 truncate">{ description }</h2>
+      <section className="p-5 grid items-center h-28 lg:h-auto">
+        <h1 className="font-ocr text-xl lg:text-3xl pb-1 lg:pb-5 lg:truncate">{ title }</h1>
+        <h2 className="font-ocr text-base text-gray-500 lg:truncate">{ description }</h2>
       </section>
       <section className="grid grid-cols-2">
         <ChevronButton direction={ Direction.left }
