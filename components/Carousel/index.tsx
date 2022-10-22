fecbd227470ec1918d4955202255528304870865
juliusyam <juliusworldwide@gmail.com>
2022-10-22 15:04:35 +0100

    initial creation of useCarousel hook

--- components/Carousel/index.tsx
+++ components/Carousel/index.tsx
@@ -2,6 +2,7 @@ import {ChildrenProps} from "../../models";
 import {animate, PanInfo, useMotionValue} from "framer-motion";
 import {useRef, useState, Children, useEffect} from "react";
 import {Slide} from "./Slide";
+import {useCarousel} from "../../hooks/useCarousel";
 
 interface CarouselProps extends ChildrenProps {
   interval: number,
@@ -9,76 +10,21 @@ interface CarouselProps extends ChildrenProps {
   autoPlay: boolean,
 }
 
-export function Carousel({ interval, loop, autoPlay: initialAutoPlay, children: child }: CarouselProps) {
+export function Carousel({ interval, loop, autoPlay, children: child }: CarouselProps) {
 
-  const motionValue = useMotionValue(0);
-  const ref = useRef<HTMLDivElement>(null);
+  const children = Children.toArray(child);
 
-  const [selectedIdx, setSelectedIdx] = useState(0);
-  const [autoPlay, setAutoPlay] = useState(initialAutoPlay);
-
-  const clientWidth = (ref.current?.clientWidth || 0);
-
-  const newMotionValue = -selectedIdx * clientWidth;
-
-  const handleDragStart = () => {
-    setAutoPlay(false);
-  }
-
-  const handleDragEnd = (e: Event, dragProps: PanInfo) => {
-    const { offset: { x: offsetX } } = dragProps;
-
-    if (offsetX > clientWidth / 4) {
-      goToPrev();
-    } else if (offsetX < -clientWidth / 4) {
-      goToNext();
-    } else {
-      animate(motionValue, newMotionValue, {
-        type: 'spring',
-        bounce: 0,
-      })
-    }
-
-    if (initialAutoPlay) setAutoPlay(true);
-  }
-
-  const children = Children.toArray(child)
-
-  const goToPrev = () => {
-    const idx = loop ? children.length - 1 : 0;
-    setSelectedIdx(selectedIdx - 1 < 0 ? idx : selectedIdx - 1);
-  }
-
-  const goToNext = () => {
-    const idx = loop ? 0 : selectedIdx;
-    setSelectedIdx(selectedIdx + 1 === children.length ? idx : selectedIdx + 1);
-  }
-
-  useEffect(() => {
-    const controls = animate(motionValue, newMotionValue, {
-      type: 'spring',
-      bounce: 0,
-    });
-
-    return controls.stop;
-  }, [selectedIdx]);
-
-  useEffect(() => {
-    if (!autoPlay) {
-      return
-    }
-
-    const timer = setInterval(goToNext, interval);
-    return () => clearInterval(timer);
-
-  }, [interval, goToNext, autoPlay]);
+  const { ref, motionValue, dragEvent } = useCarousel({
+    quantity: children.length,
+    config: {interval, loop, autoPlay },
+  });
 
   return (
     <div className="relative w-full h-screen overflow-x-hidden flex" ref={ ref }>
       {
         children.map((child, i) => (
-          <Slide onDragStart={ handleDragStart }
-                 onDragEnd={ handleDragEnd }
+          <Slide onDragStart={ dragEvent.handleDragStart }
+                 onDragEnd={ dragEvent.handleDragEnd }
                  motionValue={ motionValue }
                  index={ i }
                  key={ i }>
---
+++ hooks/useCarousel.ts
@@ -0,0 +1,98 @@
+import {animate, MotionValue, PanInfo, useMotionValue} from "framer-motion";
+import {Children, RefObject, useEffect, useRef, useState} from "react";
+
+interface CarouselConfig {
+  interval: number,
+  loop: boolean,
+  autoPlay: boolean,
+}
+
+interface CarouselProps {
+  quantity: number,
+  config: CarouselConfig
+}
+
+interface CarouselReturnObject {
+  ref: RefObject<HTMLDivElement>,
+  motionValue: MotionValue<number>,
+  dragEvent: {
+    handleDragStart: () => void,
+    handleDragEnd: (e: Event, dragProps: PanInfo) => void,
+  },
+  navigate: {
+    toPrevSlide: () => void,
+    toNextSlide: () => void,
+  }
+}
+
+export function useCarousel({ quantity, config }: CarouselProps): CarouselReturnObject {
+
+  const { interval, loop, autoPlay: initialAutoPlay } = config;
+
+  const motionValue = useMotionValue(0);
+  const ref = useRef<HTMLDivElement>(null);
+
+  const [selectedIdx, setSelectedIdx] = useState(0);
+  const [autoPlay, setAutoPlay] = useState(initialAutoPlay);
+
+  const clientWidth = (ref.current?.clientWidth || 0);
+
+  const newMotionValue = -selectedIdx * clientWidth;
+
+  const handleDragStart = () => {
+    setAutoPlay(false);
+  }
+
+  const handleDragEnd = (e: Event, dragProps: PanInfo) => {
+    const { offset: { x: offsetX } } = dragProps;
+
+    if (offsetX > clientWidth / 4) {
+      goToPrev();
+    } else if (offsetX < -clientWidth / 4) {
+      goToNext();
+    } else {
+      animate(motionValue, newMotionValue, {
+        type: 'spring',
+        bounce: 0,
+      })
+    }
+
+    if (initialAutoPlay) setAutoPlay(true);
+  }
+
+  const goToPrev = () => {
+    const idx = loop ? quantity - 1 : 0;
+    setSelectedIdx(selectedIdx - 1 < 0 ? idx : selectedIdx - 1);
+  }
+
+  const goToNext = () => {
+    const idx = loop ? 0 : selectedIdx;
+    setSelectedIdx(selectedIdx + 1 === quantity ? idx : selectedIdx + 1);
+  }
+
+  useEffect(() => {
+    const controls = animate(motionValue, newMotionValue, {
+      type: 'spring',
+      bounce: 0,
+    });
+
+    return controls.stop;
+  }, [selectedIdx]);
+
+  useEffect(() => {
+    if (!autoPlay) {
+      return
+    }
+
+    const timer = setInterval(goToNext, interval);
+    return () => clearInterval(timer);
+
+  }, [interval, goToNext, autoPlay]);
+
+  return {
+    ref,
+    motionValue,
+    dragEvent: { handleDragStart, handleDragEnd },
+    navigate: { toNextSlide: goToNext, toPrevSlide: goToPrev },
+  }
+}
