1930574d5874b19f2f85e260cf0c1119912084e5
juliusyam <jyyam1999@gmail.com>
2022-10-23 18:48:29 +0100

    tech stack scroll to section

--- pages/tech-stack.tsx
+++ pages/tech-stack.tsx
@@ -1,6 +1,6 @@
 import {GetStaticProps, NextPage} from "next";
 import {apiService} from "../services/ApiService";
-import { Tech, TechStackCategories} from "../models/apiModels";
+import {Tech, TechStackCategories} from "../models/apiModels";
 import {StringChildrenProps} from "../models";
 import {TechItem} from "../components/TechItem";
 
@@ -20,21 +20,32 @@ export const getStaticProps: GetStaticProps<TechStackProps> = async() => {
   }
 }
 
+interface TitleProps extends StringChildrenProps {
+  category: TechStackCategories,
+}
+
 const TechStack: NextPage<TechStackProps> = ({ techStack }) => {
 
-  const NavItem = ({ children }: StringChildrenProps) => (
-    <h1 className="font-ocr text-jy-lime text-3xl sm:text-5xl my-5 hover:drop-shadow-nav-text text-right cursor-pointer">{ children }</h1>
+  const NavItem = ({ children, category }: TitleProps) => (
+    <div onClick={ e => {
+      let anchor = document.getElementById(category);
+      e.preventDefault();
+
+      anchor && anchor.scrollIntoView({ behavior: 'smooth' });
+    } }>
+      <h1 className="font-ocr text-jy-lime text-3xl sm:text-5xl my-5 hover:drop-shadow-nav-text text-right cursor-pointer">{ children }</h1>
+    </div>
   )
 
   return (
     <div className="grid grid-cols-2 gap-10 w-full h-screen">
       <div className="hidden md:grid justify-items-end items-center">
         <div className="grid">
-          <NavItem>Web</NavItem>
-          <NavItem>Mobile</NavItem>
-          <NavItem>Design</NavItem>
-          <NavItem>Backend</NavItem>
-          <NavItem>Language</NavItem>
+          <NavItem category={ TechStackCategories.web }>Web</NavItem>
+          <NavItem category={ TechStackCategories.mobile }>Mobile</NavItem>
+          <NavItem category={ TechStackCategories.design }>Design</NavItem>
+          <NavItem category={ TechStackCategories.backend }>Backend</NavItem>
+          <NavItem category={ TechStackCategories.language }>Language</NavItem>
         </div>
       </div>
 
@@ -47,13 +58,16 @@ const TechStack: NextPage<TechStackProps> = ({ techStack }) => {
 
 export function TechStackList({ techStack }: TechStackProps) {
 
-  const Title = ({ children }: StringChildrenProps) => (
-    <h4 className="font-ocr text-1xl sm:text-2xl text-jy-yellow mt-16 mb-4 text-jy-lime">{ children }</h4>
+  const Title = ({ children, category }: TitleProps) => (
+    <h4 id={ category }
+        className="font-ocr text-1xl sm:text-2xl text-jy-yellow pt-16 mb-4 text-jy-lime">
+      { children }
+    </h4>
   )
 
   return (
     <div className="md:p-20 z-10 w-full">
-      <Title>Web</Title>
+      <Title category={ TechStackCategories.web }>Web</Title>
       <>
         {
           techStack.filter(t => t.attributes.category === TechStackCategories.web).map(t =>
@@ -62,7 +76,7 @@ export function TechStackList({ techStack }: TechStackProps) {
         }
       </>
 
-      <Title>Mobile</Title>
+      <Title category={ TechStackCategories.mobile }>Mobile</Title>
       <>
         {
           techStack.filter(t => t.attributes.category === TechStackCategories.mobile).map(t =>
@@ -71,7 +85,7 @@ export function TechStackList({ techStack }: TechStackProps) {
         }
       </>
 
-      <Title>Design</Title>
+      <Title category={ TechStackCategories.design }>Design</Title>
       <>
         {
           techStack.filter(t => t.attributes.category === TechStackCategories.design).map(t =>
@@ -80,7 +94,7 @@ export function TechStackList({ techStack }: TechStackProps) {
         }
       </>
 
-      <Title>Backend</Title>
+      <Title category={ TechStackCategories.backend }>Backend</Title>
       <>
         {
           techStack.filter(t => t.attributes.category === TechStackCategories.backend).map(t =>
@@ -89,7 +103,7 @@ export function TechStackList({ techStack }: TechStackProps) {
         }
       </>
 
-      <Title>Language</Title>
+      <Title category={ TechStackCategories.language }>Language</Title>
       <>
         {
           techStack.filter(t => t.attributes.category === TechStackCategories.language).map(t =>
