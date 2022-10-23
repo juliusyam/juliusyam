96b1cda3001083b8d0e6b642e596893d0683d6a9
juliusyam <jyyam1999@gmail.com>
2022-10-23 18:31:57 +0100

    updated tech stack look and feel

--- components/TechItem.tsx
+++ components/TechItem.tsx
@@ -1,4 +1,4 @@
-import { Tech } from '../models/apiModels';
+import {ExperienceLevel, Tech} from '../models/apiModels';
 import Image from 'next/image';
 
 interface TechItemProps {
@@ -18,9 +18,34 @@ export function TechItem({ tech }: TechItemProps) {
         />
       </div>
 
-      <div>
-        <h4 className="font-ocr text-2xl">{ tech.attributes.title }</h4>
+      <div className="grid w-1/2">
+        <h4 className="font-ocr text-2xl mb-4">{ tech.attributes.title }</h4>
+        <ExperienceIndicator experienceLevel={ tech.attributes.experienceLevel } />
       </div>
     </div>
+  )
+}
+
+function ExperienceIndicator({ experienceLevel }: { experienceLevel: ExperienceLevel }) {
+
+  let color: string;
+  let width: string;
+
+  switch (experienceLevel) {
+    case ExperienceLevel.limited:
+      color = 'bg-jy-red';
+      width = 'w-1/3';
+      break;
+    case ExperienceLevel.moderate:
+      color = 'bg-jy-lime';
+      width = 'w-2/3';
+      break;
+    case ExperienceLevel.proficient:
+      color = 'bg-jy-green';
+      width = 'w-full';
+  }
+
+  return (
+    <div className={`h-2 rounded-full ${ width } ${ color }`} />
   )
 }
--- pages/tech-stack.tsx
+++ pages/tech-stack.tsx
@@ -22,12 +22,19 @@ export const getStaticProps: GetStaticProps<TechStackProps> = async() => {
 
 const TechStack: NextPage<TechStackProps> = ({ techStack }) => {
 
+  const NavItem = ({ children }: StringChildrenProps) => (
+    <h1 className="font-ocr text-jy-lime text-3xl sm:text-5xl my-5 hover:drop-shadow-nav-text text-right cursor-pointer">{ children }</h1>
+  )
+
   return (
     <div className="grid grid-cols-2 gap-10 w-full h-screen">
       <div className="hidden md:grid justify-items-end items-center">
         <div className="grid">
-          <h1>Web</h1>
-          <h1>Mobile</h1>
+          <NavItem>Web</NavItem>
+          <NavItem>Mobile</NavItem>
+          <NavItem>Design</NavItem>
+          <NavItem>Backend</NavItem>
+          <NavItem>Language</NavItem>
         </div>
       </div>
 
@@ -41,15 +48,51 @@ const TechStack: NextPage<TechStackProps> = ({ techStack }) => {
 export function TechStackList({ techStack }: TechStackProps) {
 
   const Title = ({ children }: StringChildrenProps) => (
-    <h4 className="font-ocr text-1xl sm:text-2xl text-jy-yellow mb-4 text-jy-lime">{ children }</h4>
+    <h4 className="font-ocr text-1xl sm:text-2xl text-jy-yellow mt-16 mb-4 text-jy-lime">{ children }</h4>
   )
 
   return (
-    <div className="mb:p-20 z-10 w-full">
+    <div className="md:p-20 z-10 w-full">
       <Title>Web</Title>
       <>
         {
           techStack.filter(t => t.attributes.category === TechStackCategories.web).map(t =>
+            <TechItem tech={ t } key={ t.id } />
+          )
+        }
+      </>
+
+      <Title>Mobile</Title>
+      <>
+        {
+          techStack.filter(t => t.attributes.category === TechStackCategories.mobile).map(t =>
+            <TechItem tech={ t } key={ t.id } />
+          )
+        }
+      </>
+
+      <Title>Design</Title>
+      <>
+        {
+          techStack.filter(t => t.attributes.category === TechStackCategories.design).map(t =>
+            <TechItem tech={ t } key={ t.id } />
+          )
+        }
+      </>
+
+      <Title>Backend</Title>
+      <>
+        {
+          techStack.filter(t => t.attributes.category === TechStackCategories.backend).map(t =>
+            <TechItem tech={ t } key={ t.id } />
+          )
+        }
+      </>
+
+      <Title>Language</Title>
+      <>
+        {
+          techStack.filter(t => t.attributes.category === TechStackCategories.language).map(t =>
             <TechItem tech={ t } key={ t.id } />
           )
         }
