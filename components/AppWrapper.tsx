c0af513b261a72f4c7081cf1bb7274f1f01f80aa
juliusyam <juliusworldwide@gmail.com>
2022-10-30 18:27:26 +0000

    updated html head

--- components/AppWrapper.tsx
+++ components/AppWrapper.tsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 import { AnimatePresence } from 'framer-motion';
 import { ChildrenProps } from '../models';
 import Head from "next/head";
+import {Html} from "next/document";
 import {ActivityContext} from "../contexts/ActivityContext";
 import {useActivityContextState} from "../contexts/ActivityContextState";
 
@@ -29,15 +30,17 @@ function Wrapper({ children }: ChildrenProps) {
 
   return (
     <ActivityContext.Provider value={ activityState }>
-      <Head>
-        <title>Julius Yam | Web, Mobile, UX, Graphic Design</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
-        <meta name="description" content="Design is a function that transcends practicality into the world of beauty. The idea of a design focused software developer is to create bespoke software products that converts functionality into aesthetics and usability, whilst devising a design language uniquely tailored to each client, and ultimately transform unimaginable ideas to life."/>
-        <link rel="shortcut icon" href="/favicon.ico" />
-      </Head>
-      <AnimatePresence exitBeforeEnter initial={ false }>
-        { children }
-      </AnimatePresence>
+      <Html className="dark">
+        <Head>
+          <title>Julius Yam | Web, Mobile, UX, Graphic Design</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
+          <meta name="description" content="Design is a function that transcends practicality into the world of beauty. The idea of a design focused software developer is to create bespoke software products that converts functionality into aesthetics and usability, whilst devising a design language uniquely tailored to each client, and ultimately transform unimaginable ideas to life."/>
+          <link rel="shortcut icon" href="/favicon.ico" />
+        </Head>
+        <AnimatePresence exitBeforeEnter initial={ false }>
+          { children }
+        </AnimatePresence>
+      </Html>
     </ActivityContext.Provider>
   )
 }
