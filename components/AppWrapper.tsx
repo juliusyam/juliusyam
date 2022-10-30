8d2350b2782b3d192e8563065fb175fa40785bf7
juliusyam <juliusworldwide@gmail.com>
2022-10-30 18:32:32 +0000

    updated AppWrapper html class dark

--- components/AppWrapper.tsx
+++ components/AppWrapper.tsx
@@ -3,7 +3,6 @@ import { useRouter } from 'next/router';
 import { AnimatePresence } from 'framer-motion';
 import { ChildrenProps } from '../models';
 import Head from "next/head";
-import {Html} from "next/document";
 import {ActivityContext} from "../contexts/ActivityContext";
 import {useActivityContextState} from "../contexts/ActivityContextState";
 
@@ -30,17 +29,16 @@ function Wrapper({ children }: ChildrenProps) {
 
   return (
     <ActivityContext.Provider value={ activityState }>
-      <Html className="dark">
-        <Head>
-          <title>Julius Yam | Web, Mobile, UX, Graphic Design</title>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
-          <meta name="description" content="Design is a function that transcends practicality into the world of beauty. The idea of a design focused software developer is to create bespoke software products that converts functionality into aesthetics and usability, whilst devising a design language uniquely tailored to each client, and ultimately transform unimaginable ideas to life."/>
-          <link rel="shortcut icon" href="/favicon.ico" />
-        </Head>
-        <AnimatePresence exitBeforeEnter initial={ false }>
-          { children }
-        </AnimatePresence>
-      </Html>
+      <Head>
+        <html className="dark" />
+        <title>Julius Yam | Web, Mobile, UX, Graphic Design</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
+        <meta name="description" content="Design is a function that transcends practicality into the world of beauty. The idea of a design focused software developer is to create bespoke software products that converts functionality into aesthetics and usability, whilst devising a design language uniquely tailored to each client, and ultimately transform unimaginable ideas to life."/>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <AnimatePresence exitBeforeEnter initial={ false }>
+        { children }
+      </AnimatePresence>
     </ActivityContext.Provider>
   )
 }
