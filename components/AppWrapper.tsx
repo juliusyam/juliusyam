5952ccae98808f6114395c9eb6383e50a3caee1b
juliusyam <juliusworldwide@gmail.com>
2024-09-24 14:31:09 +0100

    Updated head title

--- components/AppWrapper.tsx
+++ components/AppWrapper.tsx
@@ -25,7 +25,7 @@ function Wrapper({ children }: ChildrenProps) {
   return (
     <ActivityContext.Provider value={ activityState }>
       <Head>
-        <title>Julius Yam | Web, Mobile, UX, Graphic Design</title>
+        <title>Julius Yam | FullStack Web, iOS, Android, UX & Graphic Design</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
         <meta name="description" content="Design is a function that transcends practicality into the world of beauty. The idea of a design focused software developer is to create bespoke software products that converts functionality into aesthetics and usability, whilst devising a design language uniquely tailored to each client, and ultimately transform unimaginable ideas to life."/>
         <link rel="shortcut icon" href="/favicon.ico" />
